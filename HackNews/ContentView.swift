57f1f618ff4c41ff262f0ec8ec6f7877044983ff
omercsx <omercagris@gmail.com>
2025-03-29 08:46:02 -0400

    Initial Commit

---
+++ HackNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackNews
+//
+//  Created by Omer Cagri Sayir on 29.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackNews/HackNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsApp.swift
+//  HackNews
+//
+//  Created by Omer Cagri Sayir on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
